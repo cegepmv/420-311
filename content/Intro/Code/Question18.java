362bad8fac4b806c334c25b0fa632a8b13b4e4c3
la-sarita <sara.boumehraz@collegemv.qc.ca>
2025-08-25 16:25:46 -0400

    Masquer les exercices théoriques pour diagnostique

---
+++ content/Intro/Code/Question18.java
@@ -0,0 +1,13 @@
+public class Question18 {
+    public static void main (String args[]) {
+        int param1, param2= 1;
+       param1= 10;
+       if (param1!= 10 && param1/ 0 == 0) {
+           System.out.println(param2);
+       }
+       else {
+          System.out.println(++param2);
+       }
+            }
+
+}
